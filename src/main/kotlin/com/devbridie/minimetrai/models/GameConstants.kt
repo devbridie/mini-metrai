ced10f8cbbb1ddbe83172f598a68dc8ff6728883
Dereck Bridie <me@devbridie.com>
2016-11-05 01:59:40 +0100

    Add game models

---
+++ src/main/kotlin/com/devbridie/minimetrai/models/GameConstants.kt
@@ -0,0 +1,10 @@
+package com.devbridie.minimetrai.models
+
+object GameConstants {
+    val crossingPenalty: Double = TODO()
+    val maxTrainsPerLine: Int = 4
+    val maxLines: Int = 7
+
+    val days: Array<String> = arrayOf("MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN")
+}
+
---
+++ src/main/kotlin/com/devbridie/minimetrai/models/GameState.kt
@@ -0,0 +1,43 @@
+package com.devbridie.minimetrai.models
+
+data class Position(val x: Double, val y: Double)
+enum class StationType {
+    CIRCLE,
+    TRIANGLE,
+    SQUARE,
+    STAR,
+    PENTAGON,
+    DROP,
+    FOOTBALL,
+}
+
+data class Passenger(val destination: StationType)
+data class Station(val position: Position, val stationType: StationType, val gameOverTimer: Double, val interchange: Boolean, val passengers: List<Passenger>)
+class Line()
+
+
+enum class TrainType {
+    REGULAR,
+    SHINKANSEN
+}
+
+class Inventory(val trains: Int, val specialTrains: Int, val carriages: Int, val crossings: Int)
+class Crossing()
+
+class Graph(val stations: List<Station>, val placedLines: List<PlacedLine>) {
+    class PlacedLine(val passingStations: List<Station>) {
+        sealed class Carrier(val capacity: Int, val passengers: List<Passenger>) {
+            class Train(val type: TrainType, capacity: Int, passengers: List<Passenger>, val carriages: List<Carriage>) : Carrier(capacity, passengers)
+            class Carriage(capacity: Int, passengers: List<Passenger>) : Carrier(capacity, passengers)
+        }
+    }
+}
+
+
+data class GameState(
+        val inventory: Inventory,
+        val graph: Graph,
+        val passengersTransported: Int
+)
+
+data class DateTime(val day: Int, val time: Double)
