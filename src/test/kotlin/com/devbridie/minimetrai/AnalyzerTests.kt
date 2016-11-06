e6e4d3da8864d9bf64a91c8b0f2682b4879d0ad9
Dereck Bridie <me@devbridie.com>
2016-11-06 17:14:13 +0100

    Flatten tests. Fixes #1.

--- src/test/kotlin/com/devbridie/minimetrai/AnalyzerTests.kt
+++ src/test/kotlin/com/devbridie/minimetrai/AnalyzerTests.kt
@@ -2,7 +2,6 @@ package com.devbridie.minimetrai
 
 import com.devbridie.minimetrai.cv.Analyzer
 import com.devbridie.minimetrai.models.GameState
-import com.devbridie.minimetrai.models.Inventory
 import com.devbridie.minimetrai.models.Passenger
 import com.devbridie.minimetrai.models.StationType
 import io.kotlintest.specs.FreeSpec
@@ -15,25 +14,43 @@ class AnalyzerTests : FreeSpec() {
         "should analyze simple screenshot correctly" - {
             val state = screenshot("start")
 
-            "should analyze inventory correctly" {
-                state.inventory shouldBe Inventory(
-                        carriages = 0,
-                        trains = 3,
-                        crossings = 4,
-                        specialTrains = 0
-                )
+            "should analyze inventory correctly" - {
+                "should identify 0 carriages" {
+                    state.inventory.carriages shouldBe 0
+                }
+                "should identify 3 trains" {
+                    state.inventory.trains shouldBe 3
+                }
+                "should identify 4 crossings" {
+                    state.inventory.crossings shouldBe 4
+                }
+                "should identify 0 special trains" {
+                    state.inventory.specialTrains shouldBe 0
+                }
             }
 
-            "should analyze stations correctly" - {
-                "should identify all stations" {
+            "should analyze graph correctly" - {
+                "should identify three stations" {
                     state.graph.stations.size shouldBe 3
-                    state.graph.stations.map { stations -> stations.stationType }
-                            .containsAll(listOf(StationType.CIRCLE, StationType.SQUARE, StationType.TRIANGLE)) shouldBe true
                 }
-                "should identify passenger waiting at Circle" {
+                "should be able to identify one circle station" {
+                    state.graph.stations.count { station -> station.stationType == StationType.CIRCLE } shouldBe 1
+                }
+                "should be able to identify one square station" {
+                    state.graph.stations.count { station -> station.stationType == StationType.SQUARE } shouldBe 1
+                }
+                "should be able to identify one triangle station" {
+                    state.graph.stations.count { station -> station.stationType == StationType.TRIANGLE } shouldBe 1
+                }
+                "should identify square passenger waiting at circle" {
                     val circle = state.graph.stations.find { station -> station.stationType == StationType.CIRCLE }
                     (circle == null) shouldBe false
                     circle!!.passengers shouldBe listOf(Passenger(StationType.SQUARE))
+                }
+                "should analyze lines correctly" - {
+                    "should find no placed lines" {
+                        state.graph.placedLines should haveSize(0)
+                    }
                 }
             }
         }
